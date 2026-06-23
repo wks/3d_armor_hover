7b4d5f0596d1df23d1082cb0853ae2cab6481210
Kunshan Wang <wks1986@gmail.com>
2026-06-23 21:20:51 +0800

    Screenshot, README, license and metadata

--- i_functions.lua
+++ i_functions.lua
@@ -1,13 +1,19 @@
-------------------------------------------------------------
---        ___ _        __       ___        _              --
---       | __| |_  _  / _|___  / __|_ __ _(_)_ __         --
---       | _|| | || | > _|_ _| \__ \ V  V / | '  \        --
---       |_| |_|\_, | \_____|  |___/\_/\_/|_|_|_|_|       --
---          |__/                                          --
---                   Crouch and Climb                     --
-------------------------------------------------------------
---                      Functions                         --
-------------------------------------------------------------
+-- 3D Armor Hovering Animations
+-- Copyright (C) 2020,2022  sirrobzeroone
+-- Copyright (C) 2026  Kunshan Wang
+--
+-- This library is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU Lesser General Public
+-- License as published by the Free Software Foundation; either
+-- version 2.1 of the License, or (at your option) any later version.
+--
+-- This library is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+-- Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public
+-- License along with this library; if not, see <https://www.gnu.org/licenses/>.
 
 ----------------------------------------
 -- Get Player model and textures
